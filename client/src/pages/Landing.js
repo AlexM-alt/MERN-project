8ae8c1cc050ff18a6d514c674b2088e07782dee2
aleksander Micyk <aleksander.micyk@gmail.com>
2022-04-03 18:51:59 +0200

    add register page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,10 +1,27 @@
-import logo from "../assets/images/logo.svg";
-import main from "../assets/images/main.svg";
+
+import main from "../assets/images/main-alternative.svg";
+import Wrapper from "../assets/wrappers/LandingPage.js";
+import {Logo} from "../components"
+import {Link} from "react-router-dom"
 const Landing = () => {
   return (
-    <main>
-      <nav></nav>
-    </main>
+    <Wrapper>
+      <nav>
+        <Logo/>
+      </nav>
+      <div className="container page">
+      {/* info */}
+        <div className="info">
+          <h1>job <span>tracking</span> app</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At, temporibus ipsum, voluptatibus aut, eveniet eligendi ex saepe in voluptas quod mollitia doloribus cupiditate vel et a veniam aspernatur quasi aliquam tenetur nisi. Quidem aspernatur quae autem dolorem recusandae similique adipisci?
+          </p>
+          <Link to="/register" className="btn btn-hero">Login/Register</Link>
+        </div>
+        <img src={main} alt="job hunt"  className="img main-img"/>
+      </div>
+    </Wrapper>
   );
 };
+
 export default Landing;
